070d91a1891f3186d3eb137035a3302851d28dee
abhaydiwan <abhaydiwan@gmail.com>
2019-06-14 14:49:02 -0400

    Update main.tf

--- terraform/env/dev/create-ec2-instance/main.tf
+++ terraform/env/dev/create-ec2-instance/main.tf
@@ -3,13 +3,14 @@ provider "aws" {
   region = "us-east-1"
 }
 
-terraform {
-backend "s3"{
-   bucket= "abhay-test"
-   key = "sandbox/test/ec2instance"
-   region = "us-east-1"
-}
-  }
+#terraform {
+#backend "s3"{
+ #  bucket= "abhay-test"
+ #  key = "sandbox/test/ec2instance"
+ #  region = "us-east-1"
+#}
+ # }
+
 module "create-ec2"
   {
 instance_type = "t2.micro"
